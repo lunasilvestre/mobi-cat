90c4a27c83902a01fd690ce1a4622646194de798
Nelson Luna Silvestre <lunasilvestre@mailbox.org>
2024-06-11 09:50:21 +0000

    Reorganized folder structure. Added `scripts/` folder for preliminary scripts.

--- delete/deploy-sqs-queues.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-aws cloudformation create-stack --stack-name mitma-sqs-queues-stack --template-body file://sqs-queues.yaml
-
-aws cloudformation wait stack-create-complete --stack-name mitma-sqs-queues-stack
-
-aws cloudformation describe-stacks --stack-name mitma-sqs-queues-stack --query "Stacks[0].Outputs"
