dc2a4604dbbde0f154f80a5d9dad333a323a0923
Nelson Luna Silvestre <lunasilvestre@mailbox.org>
2024-06-26 13:24:25 +0200

    SQS file to message publish

---
+++ untar/publish_sqs_messages.py
@@ -0,0 +1,21 @@
+import boto3
+
+# Initialize the SQS client
+sqs = boto3.client('sqs')
+
+# Define the SQS queue URL
+queue_url = 'https://sqs.eu-south-2.amazonaws.com/636901251658/mitma-untar-queue'
+
+# Load the URIs from the file
+file_path = '../logs/uris_sample.txt' #filtered_uris_correct.txt'
+
+with open(file_path, 'r') as file:
+    uris = file.readlines()
+
+# Publish each URI as a separate message
+for uri in uris:
+    response = sqs.send_message(
+        QueueUrl=queue_url,
+        MessageBody=uri.strip()
+    )
+    print(f"MessageId: {response['MessageId']} sent for URI: {uri.strip()}")
